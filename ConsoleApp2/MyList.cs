b87aee8649249f0c8d43f69d22600f54f49bbc18
SwiftKillers <yuri.silenok@gmail.com>
2021-10-13 11:23:41 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp2/MyList.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    /// <summary>
+    /// Мой класс
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class MyList<MyType>
+    {
+        Item head;
+        class Item
+        {
+            public MyType Value;
+            public Item Next;
+        }
+        /// <summary>
+        /// Метод добавления
+        /// </summary>
+        /// <param name="value"></param>
+        public void Add(MyType value)
+        {
+            if (head == null)
+            {
+                head = new Item() { Value = value };
+                return;
+            }
+            Item current = head;
+            while (current.Next != null)
+                current = current.Next;
+            current.Next = new Item() { Value = value };
+        }
+
+        public void Clear()
+        {
+            head = null;
+        }
+
+        public void RemoveFirst()
+        {
+            if (head == null) return;
+            head = head.Next;
+        }
+
+        
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> list = new List<string>();
+            list.Add("aaa");
+            list.RemoveAt(0);
+
+            MyList<string> myList = new MyList<string>();
+            myList.Add("AAA");
+            myList.Add("BBB");
+            myList.Add("CCC");
+            myList.RemoveFirst();
+            myList.Clear();
+
+            List<int> list2 = new List<int>();
+            MyList<int> myList2 = new MyList<int>();
+            myList2.Add(2);
+            myList2.Add(3);
+            myList2.Add(4);
+            myList2.RemoveFirst();
+            myList2.Clear();
+            double x = -2;
+            double result;
+            
+            try
+            {
+                result = Sqrt(x);
+            }
+            catch
+            {
+                x = -x;
+            }
+            finally
+            {
+                result = Sqrt(x);
+            }
+            Console.ReadLine();
+        }
+
+        static double Sqrt(double x)
+        {
+            if (x < 0) throw new IndexOutOfRangeException("Нельзя сотворить здесь");
+            return Math.Sqrt(x);
+        }
+    }
+}
